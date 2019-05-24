bb8c5c7c28d46bec636e6bc7c7640c7564ddd7c7
Cameron Mann <cameron.mann@cybera.ca>
2019-05-24 16:23:27 -0600

    Refactor test helpers

--- cmd/dataset_new_test.go
+++ cmd/dataset_new_test.go
@@ -2,26 +2,20 @@ package cmd
 
 import (
 	"os"
-	"os/exec"
-	"path/filepath"
-	"strings"
 	"testing"
 
 	"github.com/cybera/ccds/internal/languages"
 	"github.com/cybera/ccds/internal/paths"
+	"github.com/cybera/ccds/internal/test"
 	"github.com/spf13/viper"
 )
 
 func TestDatasetNew(t *testing.T) {
-	testDir, _ := filepath.Abs("_test")
-	if err := os.Mkdir(testDir, os.ModePerm); err != nil {
-		t.Fatalf("error creating test directory: %v", err)
+	testDir, err := test.CreateTestDir()
+	if err != nil {
+		t.Fatal(err)
 	}
 	defer os.RemoveAll(testDir)
-
-	if err := os.Chdir(testDir); err != nil {
-		t.Fatalf("error changing to test directory: %v", err)
-	}
 	defer os.Chdir("../")
 
 	for _, d := range []string{".ccds", paths.DatasetsCode()} {
@@ -35,23 +29,17 @@ func TestDatasetNew(t *testing.T) {
 			viper.Set("PrimaryLanguage", language)
 			viper.WriteConfigAs(paths.Config())
 
-			cmd := exec.Command("go", "run", "../../main.go", "dataset", "new", "titanic.csv")
-			var b strings.Builder
-			cmd.Stdout = &b
-			cmd.Stderr = &b
-			if err := cmd.Run(); err != nil {
+			output, err := test.RunCommand("dataset", "new", "titanic.csv")
+			if err != nil {
 				t.Errorf("process exited with err: %v", err)
 			}
-			t.Log("output:\n", b.String())
+			t.Log("output:\n", output)
 
-			cmd = exec.Command("go", "run", "../../main.go", "dataset", "new", "titanic_clean.csv", "-d=titanic")
-			b = strings.Builder{}
-			cmd.Stdout = &b
-			cmd.Stderr = &b
-			if err := cmd.Run(); err != nil {
+			output, err = test.RunCommand("dataset", "new", "titanic_clean.csv", "-d=titanic")
+			if err != nil {
 				t.Errorf("process exited with err: %v", err)
 			}
-			t.Log("output:\n", b.String())
+			t.Log("output:\n", output)
 		})
 	}
 }
--- cmd/init_test.go
+++ cmd/init_test.go
@@ -2,12 +2,10 @@ package cmd
 
 import (
 	"os"
-	"os/exec"
-	"path/filepath"
-	"strings"
 	"testing"
 
 	"github.com/cybera/ccds/internal/languages"
+	"github.com/cybera/ccds/internal/test"
 )
 
 func TestInit(t *testing.T) {
@@ -16,24 +14,18 @@ func TestInit(t *testing.T) {
 
 	for _, language = range languages.Supported {
 		t.Run(language, func(t *testing.T) {
-			testDir, _ := filepath.Abs("_test")
-			if err := os.Mkdir(testDir, os.ModePerm); err != nil {
-				t.Fatalf("error creating test directory: %v", err)
+			testDir, err := test.CreateTestDir()
+			if err != nil {
+				t.Fatal(err)
 			}
 			defer os.RemoveAll(testDir)
-			if err := os.Chdir(testDir); err != nil {
-				t.Fatalf("error changing to test directory: %v", err)
-			}
 			defer os.Chdir("../")
 
-			cmd := exec.Command("go", "run", "../../main.go", "init", "-n", "-f", "--author", author, "--license", license, "--language", language)
-			var b strings.Builder
-			cmd.Stdout = &b
-			cmd.Stderr = &b
-			if err := cmd.Run(); err != nil {
+			output, err := test.RunCommand("init", "-n", "-f", "--author", author, "--license", license, "--language", language)
+			if err != nil {
 				t.Errorf("process exited with err: %v", err)
 			}
-			t.Log("output:\n", b.String())
+			t.Log("output:\n", output)
 		})
 	}
 }
