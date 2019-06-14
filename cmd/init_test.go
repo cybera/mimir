5d076546b5ef0ba8b2bbe2720d3f2e79dc8802d6
Cameron Mann <cameron.mann@cybera.ca>
2019-06-14 11:46:29 -0600

    Add docker image build test

--- cmd/init_test.go
+++ cmd/init_test.go
@@ -12,6 +12,13 @@ func TestInit(t *testing.T) {
 	author := "John Doe"
 	license := "MIT"
 
+	// Make sure cached templates won't fail tests
+	os.Chdir("../")
+	if output, err := test.Run("packr2", "clean"); err != nil {
+		t.Logf("failed to clear packr2 cache: %s", output)
+	}
+	os.Chdir("cmd")
+
 	for _, language = range languages.Supported {
 		t.Run(language, func(t *testing.T) {
 			testDir, err := test.CreateTestDir()
@@ -26,6 +33,16 @@ func TestInit(t *testing.T) {
 				t.Errorf("process exited with err: %v", err)
 			}
 			t.Log("output:\n", output)
+
+			t.Run("build docker image", func(t *testing.T) {
+				output, err := test.Run("docker-compose", "build")
+
+				if err != nil {
+					t.Errorf("process exited with err: %v", err)
+				}
+
+				t.Log("output:\n", output)
+			})
 		})
 	}
 }
