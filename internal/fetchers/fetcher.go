378d244ff72e7f2a78efdd86b39634afcfc5e0c9
Alecia Vogel <hello@alecia.ca>
2019-07-15 14:37:35 -0600

    Renamed 'target' to 'from' for clarity

--- internal/fetchers/fetcher.go
+++ internal/fetchers/fetcher.go
@@ -10,10 +10,10 @@ type Fetcher interface {
 	Fetch() ([]byte, error)
 }
 
-func NewFetcher(source, target string, args interface{}) (Fetcher, error) {
-	if factory, ok := Factories[source]; ok {
-		return factory(target, args)
+func NewFetcher(name, from string, args interface{}) (Fetcher, error) {
+	if factory, ok := Factories[name]; ok {
+		return factory(from, args)
 	}
 
-	return nil, errors.New("no supported fetcher for " + source)
+	return nil, errors.New("no supported fetcher for " + name)
 }
